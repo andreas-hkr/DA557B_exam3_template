3f2f551381797d179dccd5e7d1338cadf6304124
Andreas Nilsson <andreas.nilsson@hkr.se>
2024-05-18 14:07:43 +0200

    Initial commit

---
+++ exam3.py
@@ -0,0 +1,42 @@
+def read_drone_file(filename):
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+def read_plant_register(filename):
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+def filter_one_of_each(data_sets):
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+def filter_detected_by_several(data_sets):
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+def print_set(data):
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+def print_with_translation(data, translation_dict):
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+def print_results(data_sets, translation_dict):
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+def main():
+    # Replace the pass statement below with your implementation of the function
+    pass
+
+
+if __name__ == '__main__':
+    main()
